23d1c5d626334c88deebb3eefb1051a551271466
Bunyawat <stonksfarm37204@gamil.com>
2021-05-18 00:20:30 +0700

    update to 1.1.0 version

--- example.js
+++ example.js
@@ -1,7 +1,20 @@
 import ThaiNumber_Converter from './index.js';
 
 //convert to number
-console.log(ThaiNumber_Converter.toInteger('๑,๑๑๓'));
+console.log(ThaiNumber_Converter.ThainumToInteger('๑,๑๑๓'));
+// output is 1113
 
 //convert to string
-console.log(ThaiNumber_Converter.toStringData('๑๑๒'));
+console.log(ThaiNumber_Converter.ThaiNumtoStringData('๑๑๒'));
+// output is 112
+
+//convert Integer to thai
+console.log(ThaiNumber_Converter.IntegerToThaiNumber('112'));
+console.log(ThaiNumber_Converter.IntegerToThaiNumber(112));
+// output is ๑๑๒
+
+/* Thanks to antronic to convert number to ThaiLang*/
+/* Source code https://github.com/antronic/thai-baht-text-js */
+console.log(ThaiNumber_Converter.IntegerToThaiLang(112));
+console.log(ThaiNumber_Converter.IntegerToThaiLang('112'));
+// output is หนึ่งร้อยสิบสอง
--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+import THBText from 'thai-baht-text';
+
 const ThaiNumbers_Dictionary = {
-  '0': '0',
-  '๐': '0',
-  '๑': '1',
-  '๒': '2',
-  '๓': '3',
-  '๔': '4',
-  '๕': '5',
-  '๖': '6',
-  '๗': '7',
-  '๘': '8',
-  '๙': '9',
+  "0": "0",
+  "๐": "0",
+  "๑": "1",
+  "๒": "2",
+  "๓": "3",
+  "๔": "4",
+  "๕": "5",
+  "๖": "6",
+  "๗": "7",
+  "๘": "8",
+  "๙": "9",
+};
+
+const IntegerNumbers_Dictionary = {
+  0: "๐",
+  1: "๑",
+  2: "๒",
+  3: "๓",
+  4: "๔",
+  5: "๕",
+  6: "๖",
+  7: "๗",
+  8: "๘",
+  9: "๙",
 };
 
 function splitText(text, spliter) {
@@ -19,37 +34,58 @@ function splitText(text, spliter) {
 }
 
 export default class ThaiNumber_Converter {
-  static toInteger(thainumber) {
-    if (thainumber === '' || thainumber === null) return 'data is null';
-    let value = splitText(thainumber,',');
-    if (value.length === 1) value = value[0].split('');
+  static ThainumToInteger(thainumber) {
+    if (thainumber === "" || thainumber === null) return "data is null";
+    let value = splitText(thainumber, ",");
+    if (value.length === 1) value = value[0].split("");
     let new_value = [];
     for (let i = 0; i < value.length; i++) {
       if (value[i] in ThaiNumbers_Dictionary) {
         new_value[i] = ThaiNumbers_Dictionary[value[i]];
-      }
-      else {
-        return 'error check your data is it thai?';
+      } else {
+        return "error check your data is it thai?";
       }
     }
     new_value = new_value.reduce((a, b) => a + b);
     return parseInt(new_value);
   }
-  static toStringData(thainumber) {
-    if (thainumber === '' || thainumber === null) return 'data is null';
-    let value = splitText(thainumber,',');
-    if (value.length === 1) value = value[0].split('');
+  static ThaiNumtoStringData(thainumber) {
+    if (thainumber === "" || thainumber === null) return "data is null";
+    let value = splitText(thainumber, ",");
+    if (value.length === 1) value = value[0].split("");
     let new_value = [];
     for (let i = 0; i < value.length; i++) {
       if (value[i] in ThaiNumbers_Dictionary) {
         new_value[i] = ThaiNumbers_Dictionary[value[i]];
-      }
-      else {
-        return 'error check your data is it thai?';
+      } else {
+        return "error check your data is it thai?";
       }
     }
     new_value = new_value.reduce((a, b) => a + b);
     return new_value;
   }
-}
 
+  static IntegerToThaiNumber(integernumber){
+    integernumber = integernumber.toString();
+    if (integernumber === "" || integernumber === null) return "data is null";
+    let value = splitText(integernumber, ",");
+    if (value.length === 1) value = value[0].split("");
+    let new_value = [];
+    for (let i = 0; i < value.length; i++) {
+      if (value[i] in IntegerNumbers_Dictionary) {
+        new_value[i] = IntegerNumbers_Dictionary[value[i]];
+      } else {
+        return "error check your data is it integer";
+      }
+    }
+    new_value = new_value.reduce((a, b) => a + b);
+    return new_value;
+  }
+
+  static IntegerToThaiLang(number){
+    if(typeof (number) === 'string') number = parseInt(number);
+    let str = THBText(number);
+    let value = str.split("บาทถ้วน");
+    return value[0];
+  }
+}
