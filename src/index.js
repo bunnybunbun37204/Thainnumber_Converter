7026008af84118322f7e85d518f9d2d4b19fefda
Bunyawat <stonksfarm37204@gmail.com>
2021-05-24 19:44:22 +0700

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-  ThaiNumToInteger : require('../src/ThainumToInteger'),
-  ThaiNumToStringData : require('../src/ThaiNumtoStringData'),
-  ThaiNumToEnglish : require('../src/ThaiNumToEnglish'),
-  IntegerToThaiNumber : require('../src/IntegerToThaiNumber'),
-  IntegerToThaiLang : require('../src/IntegerToThaiLang'),
-  IntegerToEnglish : require('../src/IntegerToEnglish')
+  ThaiNumToInteger: require('../src/ThainumToInteger'),
+  ThaiNumToStringData: require('../src/ThaiNumtoStringData'),
+  ThaiNumToEnglish: require('../src/ThaiNumToEnglish'),
+  IntegerToThaiNumber: require('../src/IntegerToThaiNumber'),
+  IntegerToThaiLang: require('../src/IntegerToThaiLang'),
+  IntegerToEnglish: require('../src/IntegerToEnglish'),
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,6 @@
+/* eslint-disable max-len */
+/* eslint-disable new-cap */
+/* eslint-disable camelcase */
 const ThaiNumber_Converter = require('../src/index');
 const assert = require('assert');
 
@@ -7,17 +10,17 @@ assert.strictEqual(ThaiNumber_Converter.ThaiNumToStringData('๑๑๒'), '112')
 console.log(`\u001B[32m✓\u001B[39m Tests 2 passed`);
 assert.strictEqual(ThaiNumber_Converter.IntegerToThaiLang('9,999,999,999,999'), 'เก้าแสนเก้าหมื่นเก้าพันเก้าร้อยเก้าสิบเก้าล้านเก้าแสนเก้าหมื่นเก้าพันเก้าร้อยเก้าสิบเก้า');
 console.log(`\u001B[32m✓\u001B[39m Tests 3 passed`);
-assert.strictEqual(ThaiNumber_Converter.IntegerToEnglish(112,'ordinal'),'112th');
+assert.strictEqual(ThaiNumber_Converter.IntegerToEnglish(112, 'ordinal'), '112th');
 console.log(`\u001B[32m✓\u001B[39m Tests 4 passed`);
-assert.strictEqual(ThaiNumber_Converter.IntegerToThaiNumber(112),'๑๑๒');
+assert.strictEqual(ThaiNumber_Converter.IntegerToThaiNumber(112), '๑๑๒');
 console.log(`\u001B[32m✓\u001B[39m Tests 5 passed`);
-assert.strictEqual(ThaiNumber_Converter.IntegerToThaiLang(1),'หนึ่ง');
+assert.strictEqual(ThaiNumber_Converter.IntegerToThaiLang(1), 'หนึ่ง');
 console.log(`\u001B[32m✓\u001B[39m Tests 6 passed`);
-assert.strictEqual(ThaiNumber_Converter.ThaiNumToEnglish('๑๑๒','ordinal'),'112th');
+assert.strictEqual(ThaiNumber_Converter.ThaiNumToEnglish('๑๑๒', 'ordinal'), '112th');
 console.log(`\u001B[32m✓\u001B[39m Tests 7 passed`);
-assert.strictEqual(ThaiNumber_Converter.IntegerToThaiLang('1000101'),'หนึ่งล้านหนึ่งร้อยเอ็ด');
+assert.strictEqual(ThaiNumber_Converter.IntegerToThaiLang('1000101'), 'หนึ่งล้านหนึ่งร้อยเอ็ด');
 console.log(`\u001B[32m✓\u001B[39m Tests 8 passed`);
-assert.strictEqual(ThaiNumber_Converter.IntegerToThaiLang('101'),'หนึ่งร้อยเอ็ด');
+assert.strictEqual(ThaiNumber_Converter.IntegerToThaiLang('101'), 'หนึ่งร้อยเอ็ด');
 console.log(`\u001B[32m✓\u001B[39m Tests 9 passed`);
-assert.strictEqual(ThaiNumber_Converter.IntegerToThaiLang('100,000,000,000,000,000'),'หนึ่งแสนล้านล้าน');
+assert.strictEqual(ThaiNumber_Converter.IntegerToThaiLang('100,000,000,000,000,000'), 'หนึ่งแสนล้านล้าน');
 console.log(`\u001B[32m✓\u001B[39m Tests 10 passed`);
